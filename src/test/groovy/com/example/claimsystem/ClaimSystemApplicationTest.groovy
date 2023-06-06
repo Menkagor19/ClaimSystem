dadf5b5f5cd3d3b949a8dc81b7a1f8d9e451f680
Menkagor19 <menka.gorakhapuriya@thoughtworks.com>
2023-06-06 12:47:58 +0530

    Initial commit

---
+++ src/test/groovy/com/example/claimsystem/ClaimSystemApplicationTest.groovy
@@ -0,0 +1,10 @@
+package com.example.claimsystem
+
+import spock.lang.Specification
+
+class ClaimSystemApplicationTest extends Specification{
+    def "Test"(){
+        expect:
+        1+1==2
+    }
+}
